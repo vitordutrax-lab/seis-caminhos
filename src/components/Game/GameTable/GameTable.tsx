8bec59f4514ac0316b393bfe80b000a23c691b89
Victor <vitor.dutrax@gmail.com>
2026-06-10 15:25:31 -0300

    tarde 10/06

--- src/components/Game/GameTable/GameTable.tsx
+++ src/components/Game/GameTable/GameTable.tsx
@@ -2,15 +2,30 @@ import './GameTable.css'
 
 type GameTableProps = {
   children: React.ReactNode
+
+  terrain:
+    | 'fire'
+    | 'water'
+    | 'earth'
+    | 'air'
+    | 'light'
+    | 'darkness'
+    | 'heroes'
 }
 
 export function GameTable({
   children,
+  terrain,
 }: GameTableProps) {
   return (
     <div className="game-table">
 
-      <div className="game-table-board" />
+      <div
+        className={`
+          game-table-board
+          terrain-${terrain}
+        `}
+      />
 
       {children}
 
